4c88074ec1d1906f96774221320a46f31ca71e53
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-01-16 14:47:27 -0500

    font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
 	content: ["./src/**/*.tsx"],
 	theme: {
 		extend: {
 			fontFamily: {
-				//sans: ["var(--font-geist-sans)", ...fontFamily.sans]
+				sans: ["neue-haas-grotesk-text", ...fontFamily.sans],
+				display: ["neue-haas-grotesk-display", ...fontFamily.sans]
 			}
 		}
 	},
