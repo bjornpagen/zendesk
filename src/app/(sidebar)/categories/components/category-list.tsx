bbf34e72f37ee983f5615f8bb880a90c228c1968
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-01-27 11:31:45 -0500

    category grid

--- src/app/(sidebar)/categories/components/category-list.tsx
+++ src/app/(sidebar)/categories/components/category-list.tsx
@@ -27,7 +27,7 @@ export function CategoryList({ initialCategories }: CategoryListProps) {
 				</Button>
 			</div>
 
-			<div className="grid gap-4">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{categories.map((category) => (
 					<CategoryCard
 						key={category.id}
